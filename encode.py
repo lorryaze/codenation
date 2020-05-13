4af85c85dc6a0a654ade102e00b1638533b7c7a2
lorryaze <lorrany90@gmail.com>
2020-05-13 01:43:41 -0300

    Criando o a função da que decodifica a cifra de cesar

--- encode.py
+++ encode.py
@@ -9,11 +9,33 @@ import json
 def write_json(obj):
     with open('answer.json', 'w') as f:
         json.dump(obj, f)
+
 #open for reading
 def read_json(file):
     with open('answer.json', 'r') as f:
         return json.load(f)
 
+#Decoded message function
+def decode(str, number):
+    i = 0
+    arr = []
+    for s in str:
+        aux = ord(s)
+        if(aux >= 97  and aux <= 122):
+            pos = aux - 97
+            pos -= number
+            if(pos < 0):
+                pos += 26
+            sub = 97 + pos
+            arr.append(chr(sub))
+        else:
+            sub = aux
+            arr.append(chr(sub))
+        i += 1
+    decoded = ''.join(arr)
+    #print(decoded)
+    return decoded
+            
 # api-endpoint
 URL = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=cb782ac5c5013d7fa2f34906b7a68702e9a79e5b'
 
@@ -24,9 +46,22 @@ r = requests.get(url = URL,
 
 #extracting data in json format 
 data = r.json()
+number = data['numero_casas']
+encrypted = data['cifrado']
 
-#read the data and create a json file called answer.json and put the data in this file 
+#call the decoded message function
+decoded = decode(encrypted, number)
+
+#inserting the decoded message int the data dict
+data['decifrado'] = decoded
+
+#read the data(dict) and create a json file called answer.json and put the data in this file 
 write_json(data)
 
 #print the answer.json
-print(read_json('answer.json'))
+#print("Response status: ", r)
+#print("Response in dict form: ", data)
+print("Answer file: ", read_json('answer.json'))
+#print("Number of 'numero_casas': ", number)
+print("Encrypted message: ", encrypted)
+print("Decoded message: ", decoded)
